0323fa14a26248f9881f55f1b4de29810a3e641e
Álvaro Villamarín <alvaro.villamarin@agiliacenter.com>
2024-09-03 18:42:20 +0200

    Add only superadmins can access users route

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { User } from 'src/users/entities/user.entity';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
